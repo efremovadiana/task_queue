ebedd3ce14fda046f17d5c41e35306d3879ffaf4
efremovadiana <136587564+efremovadiana@users.noreply.github.com>
2026-05-15 23:14:37 +0300

    Update tasks.cpp

--- tasks.cpp
+++ tasks.cpp
@@ -5,53 +5,49 @@
 
 using namespace std;
 
-PrintTask::PrintTask(const string& msg): message(msg){}
+PrintTask::PrintTask(const string& msg) : message(msg) {}
 
-void PrintTask:: execute(){
-
-    cout <<"PrintTask: "<< message << endl;
+void PrintTask::execute() {
+    cout << "PrintTask: " << message << endl;
 }
 
+RandomTask::RandomTask(int minVal, int maxVal) : minValue(minVal), maxValue(maxVal) {}
 
-RandomTask::RandomTask(int minVal, int maxVal): minValue(minVal), maxValue(maxVal){}
-
-void RandomTask:: execute(){
-
+void RandomTask::execute() {
     srand(time(nullptr));
     int randomNum = rand() % (maxValue - minValue + 1) + minValue;
-
-    cout << "RandomTask: Cгенерировано число: " << randomNum << " (диапазон " << minValue << "-" << maxValue << ")" << endl;
+    cout << "RandomTask: Сгенерировано число: " << randomNum
+         << " (диапазон " << minValue << "-" << maxValue << ")" << endl;
 }
 
+CalculationTask::CalculationTask(int x, int y, char op) : a(x), b(y), operation(op) {}
 
-CalculationTask::CalculationTask(int x, int y, char op): a(x), b(y), operation(op) {}
-
-void CalculationTask:: execute(){
-
+void CalculationTask::execute() {
     int result = 0;
 
-    switch (operation){
-
-    case '+':
-        result = a + b;
-        break;
-
-    case '-':
-        result = a - b;
-        break;
-
-    case '*':
-        result = a * b;
-        break;
-
-    case '/':
-        if (b != 0) result = a / b;
-        else cout << "CalculationTask: ошибка деления на ноль!" << endl;
-        break;
-
-    default:
-        cout << "CalculationTask: неизвестная операция" << endl;
+    switch (operation) {
+        case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = a - b;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            if (b != 0) {
+                result = a / b;
+            } else {
+                cout << "CalculationTask: ошибка деления на ноль!" << endl;
+                return;
+            }
+            break;
+        default:
+            cout << "CalculationTask: неизвестная операция" << endl;
+            return;
     }
 
     cout << "CalculationTask: " << a << " " << operation << " " << b << " = " << result << endl;
 }
+
